4c21472546977599d18a3855f0ad925c379f7c93
Nahuel Daima <nahuel@shiftmetrics.net>
2019-05-20 09:07:21 -0300

    [2-second-assignment] A login modal was created with vuelidate that fulfills assignment criteria. The auth state was added to the store and it is being saved on local storage for demo reassons. The login allows any username or password that match the criteria. Unique session per user was not developed.

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,37 @@
 	<v-app>
 		<Header/>
 		<v-content>
+			<Login :display="display"/>
 			<Home/>
 			<Snackbar/>
 		</v-content>
+		<Footer/>
 	</v-app>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Snackbar from "./components/SnackBar";
 import Home from "./views/Home";
+import Login from "./components/Login";
 
 export default {
 	name: "App",
 	components: {
 		Header,
+		Footer,
 		Home,
-		Snackbar
+		Snackbar,
+		Login
 	},
-	data() {
-		return {
-			//
-		};
+	computed: {
+		display() {
+			return !this.$store.getters.GET_AUTH_STATE;
+		}
 	},
 	beforeCreate() {
-		this.$store.commit("initialiseStore");
+		this.$store.commit("STORE_INIT");
 	}
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-footer height="auto" dark>
+		<v-layout justify-center row wrap>
+			<v-flex grey darken-1 lighten-3 py-3 text-xs-center white--text xs12>
+				<p class="text-xs-center footerText">
+					This APP was made as a Technical Challenge for FRONTMEN.
+					<small>2019 - Nahuel Daima</small>
+				</p>
+			</v-flex>
+		</v-layout>
+	</v-footer>
+</template>
+
+<style>
+.footerText {
+	margin: 0px 20px 0px 20px;
+}
+</style>
